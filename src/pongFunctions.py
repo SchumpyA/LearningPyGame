20cd29d76c129c9f13205107668585d841331662
Adam Schumperli <aschu042@ucr.edu>
2026-04-19 00:02:04 -0700

    added basics for pong game

---
+++ src/pongFunctions.py
@@ -0,0 +1,48 @@
+import pygame
+import random
+
+# Movement Variables
+ballXSpeed = 7 * random.choice((1, -1))
+ballYSpeed = 7 * random.choice((1, -1))
+playerSpeed = 7
+opponentSpeed = 7
+
+# Movement Functions
+def ballMovement(ball, player, opponent, screenWidth, screenHeight):
+    global ballXSpeed, ballYSpeed
+    ball.x += ballXSpeed
+    ball.y += ballYSpeed
+
+    if (ball.top <= 0 or ball.bottom >= screenHeight):
+        ballYSpeed *= -1
+    if (ball.left <= 0 or ball.right >= screenWidth):
+        ballScored(ball, screenWidth, screenHeight)
+
+    if (ball.colliderect(player) or ball.colliderect(opponent)):
+        ballXSpeed *= -1
+
+def playerMovement(player, keys, screenHeight):
+    global playerSpeed
+
+    if keys[pygame.K_w]: player.y -= playerSpeed
+    if keys[pygame.K_s]: player.y += playerSpeed
+
+    if player.top <= 0: player.top = 0
+    if player.bottom >= screenHeight: player.bottom = screenHeight
+
+def opponentAI(opponent, ball, screenHeight):
+    global opponentSpeed
+
+    if opponent.top < ball.y: opponent.y += opponentSpeed
+    if opponent.bottom > ball.y: opponent.y -= opponentSpeed
+
+    if opponent.top <= 0: opponent.top = 0
+    if opponent.bottom >= screenHeight: opponent.bottom = screenHeight
+
+# Other Functions
+
+def ballScored(ball, screenWidth, screenHeight):
+    global ballXSpeed, ballYSpeed
+    ball.center = (screenWidth/2, screenHeight/2)
+    ballYSpeed *= random.choice((1, -1))
+    ballXSpeed *= random.choice((1, -1))
---
+++ src/pongGame.py
@@ -0,0 +1,46 @@
+import pygame
+import sys
+from pongFunctions import *
+
+pygame.init()
+
+# Screen and Clock set-up
+WIDTH, HEIGHT = 1280, 760
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # makes the screen
+pygame.display.set_caption('Pong')
+clock = pygame.time.Clock()  # sets the timing
+
+# Game Components
+ball = pygame.Rect(WIDTH/2 - 15, HEIGHT/2 -15, 30, 30)
+player = pygame.Rect(WIDTH - 20, HEIGHT/2 - 70, 10, 149)
+opponent = pygame.Rect(10, HEIGHT/2 - 70, 10, 149)
+
+# Colors in game
+backgroundColor = (100, 100, 125)
+ballColor = (200,200,200)
+lineColor = (255, 255, 255)
+
+running = True
+while running:
+    clock.tick(60)  #60 fps
+
+    screen.fill(backgroundColor)    # Set up screen with components and clean line to seperate the midpoint
+    pygame.draw.ellipse(screen, ballColor, ball)
+    pygame.draw.rect(screen, ballColor, player)
+    pygame.draw.rect(screen, ballColor, opponent)
+    pygame.draw.aaline(screen, lineColor, (WIDTH/2, 0), (WIDTH/2, HEIGHT))
+
+    # Movement in game
+    keys = pygame.key.get_pressed()  # gets input from keybaord
+    ballMovement(ball, player, opponent, WIDTH, HEIGHT)
+    playerMovement(player, keys, HEIGHT)
+    opponentAI(opponent, ball, HEIGHT)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # checks if game window has been closed, if it has been stop running
+            running = False
+    
+    pygame.display.flip()
+
+pygame.quit()  # closes the game
+sys.exit()     # closes the program
